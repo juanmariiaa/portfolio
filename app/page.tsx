a40c9f7c918d5f746ce675c3f0b55aee2a9725fe
juanmariiaa <arizaserranojuanmaria@gmail.com>
2025-01-17 17:01:09 +0100

    Cambios en el home

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <div className="bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block dark:bg-opacity-0"></div>
     </main>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -10,83 +10,91 @@ import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "tween",
-              duration: 0.2,
-            }}
-          >
-            <Image
-              src="/picture.jpeg"
-              alt="Foto de Juan María"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="h-28 w-28 rounded-full object-cover 
+    <main className="flex sm:flex-row flex-col sm:items-center items-center justify-center text-left h-screen">
+      <section className="sm:order-last">
+        <motion.div
+          className="relative flex items-center justify-center"
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "tween",
+            duration: 0.2,
+          }}
+        >
+          <Image
+            src="/picture.jpeg"
+            alt="Foto de Juan María"
+            width="192"
+            height="192"
+            quality="95"
+            priority={true}
+            className="h-28 w-28 rounded-full object-cover 
               border-[0.25rem] border-white shadow-xl"
-            />
-          </motion.div>
-        </div>
-      </div>
+          />
+        </motion.div>
+      </section>
 
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
-        <span className="font-bold">Hola, soy Juan María.</span> Soy
-        <span className="font-bold"> desarrollador de software.</span> Disfruto
-        creando <span className="italic">sitios webs y aplicaciones</span>.
-        Centrado ssobre todo en <span className="underline">Java</span>.
-      </motion.h1>
-      <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.1,
-        }}
-      >
-        <Link
-          href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
-          onClick={() => {}}
+      <section className="sm:mr-20 mb-6 text-center sm:text-left">
+        <motion.div
+          className="mt-4 px-4 text-lg font-medium !leading-[1.5] sm:text-xl"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
         >
-          Contáctame aquí{" "}
-          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-        </Link>
+          <h1 className=" mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl mg">
+            <span className="font-bold">Juan María</span>
+          </h1>
+          <h1 className="mb-10  px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl">
+            <span className="">Desarrollador de software.</span>
+          </h1>
+        </motion.div>
 
-        <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
-          download
+        <motion.div
+          className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.1,
+          }}
         >
-          Descarga mi CV{" "}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+          <div className="sm:flex sm:flex-row">
+            <Link
+              href="#contact"
+              className="bg-gray-900 text-white px-7 py-3 mb-1 sm:mb-0 sm:mr-1.5 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+              onClick={() => {}}
+            >
+              Contáctame aquí{" "}
+              <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+            </Link>
 
-        <a
-          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/juan-maría-ariza-serrano"
-          target="_blank"
-        >
-          <BsLinkedin />
-        </a>
+            <a
+              className="bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+              href="/CV.pdf"
+              download
+            >
+              Descarga mi CV{" "}
+              <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+            </a>
+          </div>
 
-        <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/juanmariiaa"
-          target="_blank"
-        >
-          <FaGithubSquare />
-        </a>
-      </motion.div>
-    </section>
+          <div className="flex flex-row">
+            <a
+              className="bg-white p-4 mr-1.5 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+              href="https://www.linkedin.com/in/juan-maría-ariza-serrano"
+              target="_blank"
+            >
+              <BsLinkedin />
+            </a>
+
+            <a
+              className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+              href="https://github.com/juanmariiaa"
+              target="_blank"
+            >
+              <FaGithubSquare />
+            </a>
+          </div>
+        </motion.div>
+      </section>
+    </main>
   );
 }
